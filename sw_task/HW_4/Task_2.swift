657ef0473c754cb9f66009180935b20dd2017279
MrBizzon <info.proect@mail.ru>
2023-06-13 12:01:31 +0300

    Second task

--- sw_task/HW_4/Task_2.swift
+++ sw_task/HW_4/Task_2.swift
@@ -3,18 +3,25 @@
 // Вид пиццы должен быть вложенным типом для структуры пиццы. Подсказка: добавки лучше также сделать перечислением.
 
 let name: String = "Capricciosa" //Имя
-let thickness: Double = 1 //Толщина теста
-let coefficient: Double = 1.2 //Коэффицент за толщину теста
+let coefficientOne: Double = 1.5 //Коэффицент за тонкое тесто
+let coefficientTwo: Double = 1.8 //Коэффицент за толстое тесто
 
 let sausage: Double = 100 //руб. за колбасу
 let cheese: Double = 70 //руб. за сыр
 let tomatoes: Double = 80 //руб. за томаты
 let markupOnGoods: Double = 0.4 //наценка на продукцию
 
-func getPriceRuble() -> Double {
-    let costOfPizza = (sausage + cheese + tomatoes) * coefficient
+func CostPerThinDough() -> Double {
+    let costOfPizza = (sausage + cheese + tomatoes) * coefficientOne
     return costOfPizza * markupOnGoods
 }
 
-let rub = getPriceRuble()
-print(rub)
+func CostForThickDough() -> Double {
+    let costOfPizza = (sausage + cheese + tomatoes) * coefficientTwo
+    return costOfPizza * markupOnGoods
+}
+
+let costOne = CostPerThinDough()
+print(costOne)
+let costTwo = CostForThickDough()
+print(costTwo)
