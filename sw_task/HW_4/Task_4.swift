fcc448451f112f4b0d002004ef0f34e29c82c5b7
MrBizzon <info.proect@mail.ru>
2023-06-13 12:55:02 +0300

    Fourth task

--- sw_task/HW_4/Task_4.swift
+++ sw_task/HW_4/Task_4.swift
@@ -1,2 +1,25 @@
 // Задание 4. Написать в классе пиццерии функции для добавления нового вида пиццы и для получения всех доступных пицц.
 
+class Pizza {
+    
+    static let shared = Pizza()
+    var myFirstPizza = 1
+    var mySecondPizza: Int
+    
+    init(_ pizza: Int){
+        self.mySecondPizza = myFirstPizza * 2
+        self.myMethod()
+    }
+    init(){
+        self.mySecondPizza = 0
+        self.myMethod()
+    }
+    
+    func myMethod(){
+        self.myFirstPizza += 1
+    }
+}
+
+let pizza = Pizza(5)
+print(pizza.mySecondPizza)
+print(Pizza.shared.mySecondPizza)
