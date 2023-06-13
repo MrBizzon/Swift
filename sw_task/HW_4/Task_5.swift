83bad1e7aee6d36d9bc3bda0a915c3ffac98ea66
MrBizzon <info.proect@mail.ru>
2023-06-13 12:58:07 +0300

    Fifth Task

--- sw_task/HW_4/Task_5.swift
+++ sw_task/HW_4/Task_5.swift
@@ -1,2 +1,25 @@
 // Задание 5. Создать экземпляр класса пиццерии и добавить в него несколько видов пицц.
 
+class Pizzeria {
+    
+    static let shared = Pizzeria()
+    var myFirstPizza = 1
+    var mySecondPizza: Int
+    
+    init(_ pizza: Int){
+        self.mySecondPizza = myFirstPizza * 2
+        self.myMethod()
+    }
+    init(){
+        self.mySecondPizza = 0
+        self.myMethod()
+    }
+    
+    func myMethod(){
+        self.myFirstPizza += 1
+    }
+}
+
+let pizza = Pizzeria(5)
+print(pizza.mySecondPizza)
+print(Pizzeria.shared.mySecondPizza)
