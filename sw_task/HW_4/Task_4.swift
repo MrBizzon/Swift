0d0d15d2f2929d97f9992a5d1bd30c908fc4f9bc
MrBizzon <info.proect@mail.ru>
2023-06-13 13:04:37 +0300

    Fourth task

--- sw_task/HW_4/Task_4.swift
+++ sw_task/HW_4/Task_4.swift
@@ -1,2 +1,18 @@
 // Задание 4. Написать в классе пиццерии функции для добавления нового вида пиццы и для получения всех доступных пицц.
 
+class PizzaWithSheath : Pizza {
+    
+    let priceSheath: Double
+    
+    init(_ name: String, _ thickness: Double, _ coefficient: Double, _ priceSheath: Double) {
+        self.priceSheath = priceSheath
+        super.init(name, thickness, coefficient)
+    }
+    
+    override func getPrice(_ type: Pizza.Currency) -> Double {
+        let superResult = super.getPrice(type)
+        return superResult + self.priceSheath
+    }
+}
+
+let firstPizzaWithSheath = PizzaWithSheath("Capricciosa", 14, 1.5, 1000)
