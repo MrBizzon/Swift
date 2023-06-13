f6cc317d2345b080d4349f830c8f02bc94652c04
MrBizzon <info.proect@mail.ru>
2023-06-13 13:23:35 +0300

    First task

--- sw_task/HW_4/Task_1.swift
+++ sw_task/HW_4/Task_1.swift
@@ -1,2 +1,22 @@
 // Задание 1. Создать перечисление с видами пиццы (хотя бы 4-5 кейсов).
 
+struct Pizza {
+    var name: String
+    var cost: Double    
+}
+
+func getPizza() {
+    print(\(name), \(cost))
+}
+
+var latte = Pizza(name: "Latte", cost: 110)
+var margaret = Pizza(name: "Margaret", cost: 111)
+var pepperoni = Pizza(name: "Pepperoni", cost: 111)
+var carbonara = Pizza(name: "Carbonara", cost: 111)
+
+latte.getPizza()
+margaret.getPizza()
+lapepperonitte.getPizza()
+carbonara.getPizza()
+
+
